2608f67b35cbfb640cad6fded762142aed2e0611
Clara Gaya Saura <clara@Claras-MacBook-Pro.local>
2022-02-08 14:36:56 +0000

    added screen options and reset drawer state to open when user clicks on the account tab

--- src/navigators/AccountDrawer.tsx
+++ src/navigators/AccountDrawer.tsx
@@ -1,17 +1,48 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import React from 'react';
+import { CommonActions } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { useWindowDimensions } from 'react-native';
 import { ContactPrefs } from '../screens/Account/ContactPrefs';
 import { Details } from '../screens/Account/Details';
 import { Notifications } from '../screens/Account/Notifications';
 
 const Drawer = createDrawerNavigator();
 
-export const AccountDrawer = () => {
+export const AccountDrawer = ({navigation}: any) => {
+  const {width} = useWindowDimensions();
+  // Reset state to 'open' when user clicks on the account tab
+  const unsubscribe = navigation.addListener('focus', () => {
+    navigation.dispatch(
+      CommonActions.reset({
+        index: 1,
+        routes: [
+          {name: 'My Details'},
+          {name: 'Notifications'},
+          {name: 'Contact Preferences'},
+        ]
+      })
+    )
+  })
+
+  useEffect(() => {
+    unsubscribe();
+  }, [navigation])
+
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Details" component={Details} />
+    <Drawer.Navigator
+      defaultStatus="open"
+      screenOptions={{
+        drawerPosition: 'right',
+        drawerType: width > 768 ? 'permanent' : 'front',
+        drawerStyle: width > 768 ? {width: '40%'} : {width: '100%'},
+        overlayColor: 'transparent'
+      }}
+      // drawerContent={props => <DrawerContent {...props} />}
+    >
+      <Drawer.Screen name="My Details" component={Details} />
       <Drawer.Screen name="Notifications" component={Notifications} />
-      <Drawer.Screen name="ContactPrefs" component={ContactPrefs} />
+      <Drawer.Screen name="Contact Preferences" component={ContactPrefs} />
     </Drawer.Navigator>
   );
 };
+
